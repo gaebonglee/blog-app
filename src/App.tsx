e621c1d1478eed430919d82fd491e7eb0e6e3c2e
gayoung lee <ken604@naver.com>
2025-01-10 18:11:32 +0900

    postform 오류 확인 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useContext } from "react";
-import { app } from "firebaseApp";
+import { app, db } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,6 +9,7 @@ import Routers from "components/Routers";
 import Loader from "components/Loader";
 
 function App() {
+
   const context = useContext(ThemeContext);
   const auth = getAuth(app);
   // auth를 체크하기 전에 (initialize 전)에는 loader를 띄워주는 용도

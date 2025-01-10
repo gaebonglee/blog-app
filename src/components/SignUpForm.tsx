d3b30dee15ab48b9dd12e4868313894d1f9497e0
gayoung lee <ken604@naver.com>
2025-01-10 11:41:06 +0900

    차이점 확인 중

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,30 +1,58 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUpForm() {
-  return (
-    <form action="/post" method="POST" className="form form--1g">
-      <h1 className=".form__title">회원가입</h1>
-      <div className="form__block">
-        <label htmlFor="email">아이디</label>
-        <input type="email" name="email" id="email" />
-      </div>
-      <div className="form__block">
-        <label htmlFor="password">비밀번호</label>
-        <input type="password" name="password" id="password" />
-      </div>
-      <div className="form__block">
-        <label htmlFor="password_confirm">비밀번호 확인</label>
-        <input type="password" name="password_confirm" id="password_confirm" />
-      </div>
-      <div className="form__block">
-        계정이 이미 있있으신가요?
-        <Link to="/login" className="form__link">
-          로그인 하기
-        </Link>
-      </div>
-      <div className="form__block">
-        <input type="submit" value="회원가입" className="form__btn--submit" />
-      </div>
-    </form>
-  );
+  const [error, setError] = useState<string>("")
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+  };
+  if (name === "email") {
+  }
+  if (name === "password"){
+
+  }
+  if (name === "passwordConfirm")
+    return (
+      <form action="/post" method="POST" className="form form--1g">
+        <h1 className=".form__title">회원가입</h1>
+        <div className="form__block">
+          <label htmlFor="email">아이디</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            onChange={onChange}
+          />
+        </div>
+        <div className="form__block">
+          <label htmlFor="password">비밀번호</label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <div className="form__block">
+          <label htmlFor="password_confirm">비밀번호 확인</label>
+          <input
+            type="password"
+            name="password_confirm"
+            id="password_confirm"
+            required
+          />
+        </div>
+        <div className="form__block">
+          계정이 이미 있있으신가요?
+          <Link to="/login" className="form__link">
+            로그인 하기
+          </Link>
+        </div>
+        <div className="form__block">
+          <input type="submit" value="회원가입" className="form__btn--submit" />
+        </div>
+      </form>
+    );
 }

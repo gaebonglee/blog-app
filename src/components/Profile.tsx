c3ef0d6b34ae7b940b98542ea4fe42b4da668a82
gayoung lee <ken604@naver.com>
2025-01-10 17:18:12 +0900

    firebase를 통해 AuthContextProvider 활용하여 사용자 인증 완료

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,7 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
 const onSignOut = async () => {
@@ -14,16 +16,16 @@ const onSignOut = async () => {
 };
 
 export default function Profile() {
-  const auth = getAuth(app);
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image"></div>
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
+          <div className="profile__email">{user?.email}</div>
           <div className="profile__name">
-            {auth?.currentUser?.displayName || "사용자"}
+            {user?.displayName || "사용자"}
           </div>
         </div>
       </div>
